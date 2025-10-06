04a24b04d4724fe7e27ae787d1d0fc4006b84474
karekezi fiston <karekezifiston30@gmail.com>
2025-10-06 19:28:06 +0200

    aaa

--- alx-movie-app/pages/moves/index.tsx
+++ alx-movie-app/pages/moves/index.tsx
@@ -4,7 +4,11 @@ import MovieCard from "@/components/commons/MovieCard";
 import { MoviesProps } from "@/interfaces";
 import { useCallback, useEffect, useState } from "react";
 
-const Movies: React.FC = () => {
+interface MProps {
+  movies: MoviesProps[];
+}
+
+const Movies: React.FC<MProps> = () => {
   const [page, setPage] = useState<number>(1);
   const [year, setYear] = useState<number | null>(null);
   const [genre, setGenre] = useState<string>("All");
@@ -31,7 +35,9 @@ const Movies: React.FC = () => {
     }
 
     const data = await response.json();
-    setMovies(data.movies);
+    const results = data.movies;
+    console.log(results);
+    setMovies(results);
     setLoading(false);
   }, [page, year, genre]);
 
@@ -48,29 +54,39 @@ const Movies: React.FC = () => {
             placeholder="Search for a movie..."
             className="border-2 w-full md:w-96 border-[#E2D609] outline-none bg-transparent px-4 py-2 rounded-full text-white placeholder-gray-400"
           />
+
           <select
-            onChange={(event: React.ChangeEvent<HTMLSelectElement>) => setYear(Number(event.target.value))}
+            onChange={(event: React.ChangeEvent<HTMLSelectElement>) =>
+              setYear(Number(event.target.value))
+            }
             className="border-2 border-[#E2D609] outline-none bg-transparent px-4 md:px-8 py-2 mt-4 md:mt-0 rounded-full w-full md:w-auto"
           >
             <option value="">Select Year</option>
-            {[2024, 2023, 2022, 2021, 2020, 2019].map((year) => (
-              <option value={year} key={year}>{year}</option>
+            {[2024, 2023, 2022, 2021, 2020, 2019].map((year: number) => (
+              <option value={year} key={year}>
+                {year}
+              </option>
             ))}
           </select>
         </div>
 
         <p className="text-[#E2D609] text-xl mb-6 mt-6">Online streaming</p>
         <div className="flex flex-col md:flex-row items-center justify-between">
-          <h1 className="text-lg md:text-6xl font-bold">{year} {genre} Movie List</h1>
+          <h1 className="text-lg md:text-6xl font-bold">
+            {year} {genre} Movie List
+          </h1>
           <div className="flex flex-wrap space-x-0 md:space-x-4 mt-4 md:mt-0">
-            {['All', 'Animation', 'Comedy', 'Fantasy'].map((g, key) => (
-              <Button title={g} key={key} action={() => setGenre(g)} />
-            ))}
+            {["All", "Animation", "Comedy", "Fantasy"].map(
+              (genre: string, key: number) => (
+                <Button title={genre} key={key} action={() => setGenre(genre)} />
+              )
+            )}
           </div>
         </div>
 
+        {/* Movies output */}
         <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 mt-10">
-          {movies?.map((movie, key) => (
+          {movies?.map((movie: MoviesProps, key: number) => (
             <MovieCard
               title={movie?.titleText.text}
               posterImage={movie?.primaryImage?.url}
@@ -79,9 +95,11 @@ const Movies: React.FC = () => {
             />
           ))}
         </div>
-
         <div className="flex justify-end space-x-4 mt-6">
-          <Button title="Previous" action={() => setPage(prev => prev > 1 ? prev - 1 : 1)} />
+          <Button
+            title="Previous"
+            action={() => setPage((prev) => (prev > 1 ? prev - 1 : 1))}
+          />
           <Button title="Next" action={() => setPage(page + 1)} />
         </div>
       </div>
